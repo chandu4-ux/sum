c85e660a8a3c64368b16dc0c6b277f4b0fc18c1c
chandu4-ux <hudalichandan@gmail.com>
2025-04-08 12:11:41 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Initialize variables
+        int a = 5; // First number
+        int b = 10; // Second number
+
+        // Perform addition
+        int sum = a + b;
+
+        // Display the result
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
